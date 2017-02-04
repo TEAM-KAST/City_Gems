dc5632e9883b6e5a8e5dc48eedafe5de45f246a3
groovestation31785 <email": null>
2017-02-03 16:12:51 -0800

    Add user model with username virtual attributes and omniauth method

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+# require 'faker'
+
+class User < ApplicationRecord
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
+  # I would like to give the user a username since we cannot get that from Google
+  # def username
+  #   "Faker::Pokemon.name"
+  # end
+
+  # I would also like to give them the option to change it
+  # def username=(new_username)
+  #   username = new_username
+  #   username
+  # end
+
+end
