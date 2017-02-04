3572b54699670bdcd96fb8b3a3f143004ad8b5f9
groovestation31785 <email": null>
2017-02-03 16:26:28 -0800

    Refactor original sessions controller with helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
+    @user = User.from_omniauth(env["omniauth.auth"])
+    current_user
     redirect_to root_path
   end
 
   def destroy
-    session[:user_id] = nil
+    log_out
     redirect_to root_path
   end
 end
