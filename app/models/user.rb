37d23c257fa3a8b15044a662c63bb26571dc0fbb
groovestation31785 <email": null>
2017-02-03 20:10:39 -0800

    Update user model methods for setting the user in the session

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,30 @@
 
 class User < ApplicationRecord
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
+  def self.update_or_create(auth)
+      user = User.find_by(uid: auth[:uid]) || User.new
+      user.attributes = {
+        provider: auth[:provider],
+        uid: auth[:uid],
+        email: auth[:info][:email],
+        first_name: auth[:info][:first_name],
+        last_name: auth[:info][:last_name],
+        token: auth[:credentials][:token],
+        refresh_token: auth[:credentials][:refresh_token],
+        oauth_expires_at: auth[:credentials][:expires_at]
+      }
+      user.save
+      user
     end
-  end
 
   # I would like to give the user a username since we cannot get that from Google
-  # def username
-  #   "Faker::Pokemon.name"
-  # end
+  def username
+
+  end
 
   # I would also like to give them the option to change it
-  # def username=(new_username)
-  #   username = new_username
-  #   username
-  # end
+  def username=(new_username)
+    username = new_username
+  end
 
 end
