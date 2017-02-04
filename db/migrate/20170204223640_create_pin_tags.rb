c9a823f823fee9aef6d2e17ef57ebc02ae76ce6b
groovestation31785 <email": null>
2017-02-04 15:06:53 -0800

    Fixed migration problem

--- db/migrate/20170204223640_create_pin_tags.rb
+++ db/migrate/20170204223640_create_pin_tags.rb
@@ -1,8 +1,8 @@
 class CreatePinTags < ActiveRecord::Migration[5.0]
   def change
     create_table :pin_tags do |t|
-      t.integer :pin
-      t.integer :tag
+      t.integer :pin_id
+      t.integer :tag_id
 
       t.timestamps
     end
--- db/migrate/20170204223655_create_pin_tag_votes.rb
+++ db/migrate/20170204223655_create_pin_tag_votes.rb
@@ -1,8 +1,8 @@
 class CreatePinTagVotes < ActiveRecord::Migration[5.0]
   def change
     create_table :pin_tag_votes do |t|
-      t.integer pin_tag_id
-      t.integer user_id
+      t.integer :pin_tag_id
+      t.integer :user_id
 
       t.timestamps
     end
