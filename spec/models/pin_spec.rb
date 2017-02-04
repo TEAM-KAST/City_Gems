25646f2171a0843c780e835da284e1d9fbb1a52b
groovestation31785 <email": null>
2017-02-04 15:45:13 -0800

    Retrieve coverage results from running spec

--- spec/models/pin_spec.rb
+++ spec/models/pin_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Pin, type: :model do
   describe 'validations' do
     it { is_expected.to validate_numericality_of :lat }
     it { is_expected.to validate_numericality_of :lng }
-    it { is_expected.to validate_numericality_of :creator_id }
+    it { is_expected.to validate_numericality_of :user_id }
     it { is_expected.to validate_presence_of :appeal }
   end
 
   describe 'associations' do
-    it { is_expected.to belong_to :creator }
+    it { is_expected.to belong_to :user }
     it { is_expected.to have_many :comments }
     it { is_expected.to have_many :pin_tags }
   end
