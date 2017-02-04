cc05f462e67144f837689e3052c80ae144fb1ad6
groovestation31785 <email": null>
2017-02-04 15:38:11 -0800

    Update tag models to have correct naming;tests passing

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
   describe 'tag associations' do
-    xit { is_expected.to have_many(:pintags)}
+    it { is_expected.to have_many(:pin_tags)}
     it { is_expected.to validate_presence_of(:label) }
   end
 end
