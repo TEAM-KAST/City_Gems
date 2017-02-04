621f494b3ee8ecbfdfcf368609b6974ffb6ac2a4
groovestation31785 <email": null>
2017-02-04 15:36:55 -0800

    Update models to have correct naming;tests passing

--- app/models/pin_tag_vote.rb
+++ app/models/pin_tag_vote.rb
@@ -1,7 +1,7 @@
 class PinTagVote < ApplicationRecord
-  belongs_to :pintag
+  belongs_to :pin_tag
   belongs_to :user
 
-  validates :user_id, :pintag_id, presence: true
-  validates :user_id, :pintag_id, numericality: true
+  validates :user_id, :pin_tag_id, presence: true
+  validates :user_id, :pin_tag_id, numericality: true
 end
