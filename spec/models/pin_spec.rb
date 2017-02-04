1e6b6bf6a1dd0afc90eb193b481ee0c7390518b3
groovestation31785 <angroover@gmail.com>
2017-02-04 13:19:50 -0800

    Update pin_spec.rb

--- spec/models/pin_spec.rb
+++ spec/models/pin_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Pin, type: :model do
   describe 'validations' do
     xit { is_expected.to validate_numericality_of :lat }
     xit { is_expected.to validate_numericality_of :lng }
-    xit { is_expected.to validate_numericality_of :manufacturer_id }
+    xit { is_expected.to validate_numericality_of :creator_id }
     xit { is_expected.to validate_presence_of :appeal }
   end
 
