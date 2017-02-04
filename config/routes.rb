1159dddf8c41260093caab7e785718673245b891
swwhitaker <email": null>
2017-02-04 10:17:33 -0800

    resolve merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   resource :home, only: [:show]
 
   root to: "home#show"
+
+  resources :pins
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pins#index'
 end
