b8b705007c2a1078724152ebe13c7b9155461501
groovestation31785 <email": null>
2017-02-03 19:43:51 -0800

    Update controllers to use the sessions helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     @user = User.from_omniauth(env["omniauth.auth"])
-    current_user
+    log_in @user
     redirect_to root_path
   end
 
