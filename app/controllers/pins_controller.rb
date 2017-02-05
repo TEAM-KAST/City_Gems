da2440ddb18aee4f55b3e52b822f479328d58e0a
groovestation31785 <email": null>
2017-02-05 13:23:10 -0800

    Delete home controller; change error messages for pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -28,7 +28,7 @@ class PinsController < ApplicationController
       @pin = Pin.new(pin_params)
       respond_to do |format|
         if @pin.save
-          format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
+          format.html { redirect_to @pin, notice: 'Gem was successfully created.' }
         else
           format.html { render :new }
         end
@@ -41,7 +41,7 @@ class PinsController < ApplicationController
   def update
     respond_to do |format|
       if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+        format.html { redirect_to @pin, notice: 'Gem was successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -51,7 +51,7 @@ class PinsController < ApplicationController
   def destroy
     @pin.destroy
     respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
+      format.html { redirect_to pins_url, notice: 'Gem was successfully destroyed.' }
     end
   end
 
