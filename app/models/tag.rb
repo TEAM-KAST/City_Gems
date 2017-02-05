d405e9d3b83009637e0a2af2ef59421a4fcbe02b
groovestation31785 <email": null>
2017-02-05 13:22:00 -0800

    Add tags label array to model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,9 @@
 class Tag < ApplicationRecord
   has_many :pin_tags
-  validates :label, presence: true
+
+  TAGS = %w(natural historical artistic quirky beautiful cultural)
+
+  validates :label, presence: true,
+    inclusion: { in: TAGS, message: "%{value} is not one of the tags" }
+
 end
