237ba44db585f40e0cb3881ad6296e58a0d4c6ca
swwhitaker <email": null>
2017-02-04 17:00:22 -0800

    pre-rebase commit

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem "omniauth-google-oauth2", "~> 0.2.1"
 
 gem 'bootstrap-sass', '~> 3.3.6'
 
+gem 'faker', '~> 1.7.2'
+
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+30.times {User.create(Omniauth.google)}
+200.times {
+  Pin.create(user_id: Random.rand(30), appeal: Faker::HarryPotter.quote, photo_url: Faker::Avatar.image, website: Faker::Internet.url )
+}
+300.times {
+  Comment.create(user_id: Random.rand(30), content: Faker::StarWars.quote, pin_id: Random.rand(200) )
+}
