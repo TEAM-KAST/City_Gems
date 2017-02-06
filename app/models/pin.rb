a70dc5e072f8a8b8b6ab9395b32b1d0c9448eabc
groovestation31785 <email": null>
2017-02-06 11:44:21 -0800

    Update tag model and spec to have a name/validates a name

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,14 +3,14 @@ class Pin < ApplicationRecord
   has_many :comments
   has_many :pin_tags
 
-  validates_presence_of :user_id, :lat, :lng, :appeal
+  validates_presence_of :user_id, :lat, :lng, :appeal, :name
   validates_numericality_of :user_id, :lat, :lng
 
   def self.tags(pin)
   	tag_names = ""
   	pin.pin_tags.each do |pt|
   		tag_names << "#{Tag.find_by(id: pt.tag_id).label}, "
-  	end 
+  	end
   	tag_names.chomp(', ')
   end
 
