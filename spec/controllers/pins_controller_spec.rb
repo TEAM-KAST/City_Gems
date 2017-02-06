e8f05750fbd380fe4c5ba2100a8697d9c6d0f236
groovestation31785 <email": null>
2017-02-05 20:36:59 -0800

    Create pin controller spec tests

--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe PinssController, type: :controller do
+RSpec.describe PinsController, type: :controller do
   let!(:pin) { create(:pin) }
-  let!(:comment) { create(:comment) }
-  let!(:invalid_attributes) { {content: "nothing to see here"} }
+  let!(:invalid_attributes) { {appeal: "nothing to see here"} }
 
   describe 'no user is logged in' do
-    it 'redirects users to pin show page when they try to make a new comment' do
-      get :new, params: {:pin_id => pin.id}
+    it 'redirects users to pin show page when they try to make a new pin' do
+      get :new, params: {:id => pin.to_param}
       expect(response).to redirect_to '/'
     end
 
     it 'responds with status code 302' do
-      get :new, params: {:pin_id => pin.id}
+      get :new, params: {:id => pin.to_param}
       expect(response).to have_http_status 302
     end
   end
@@ -20,52 +19,53 @@ RSpec.describe PinssController, type: :controller do
 
   describe 'when a user is logged in' do
     let!(:user) { create(:user) }
-    let!(:valid_attributes) { {user_id: user.id, content: "MyString", pin_id: pin.id} }
+    let!(:valid_attributes) { {user_id: user.id, name: "Something Pretty", appeal: "ItisreallyprettyIlikeit.", photo_url: "www.image.url",
+      website: "www.reallypretty.like", lat: 47.5979520, lng: -122.3349440} }
 
     before(:each) do
       request.session[:user_id] = user.id
     end
 
     describe "GET #new" do
-      it "assigns a new comment as @comment" do
-        get :new, params: {:pin_id => pin.id}
-        expect(assigns(:comment)).to be_a_new(Comment)
+      it "assigns a new pin as @pin" do
+        get :new, params: {:id => pin.to_param}
+        expect(assigns(:pin)).to be_a_new(Pin)
       end
     end
 
     describe "GET #edit" do
-      it "assigns the requested comment as @comment" do
-        get :edit, params: {:pin_id => pin.to_param, :id => comment.id}
-        expect(assigns(:comment)).to eq(comment)
+      it "assigns the requested pin as @pin" do
+        get :edit, params: {:id => pin.to_param}
+        expect(assigns(:pin)).to eq(pin)
       end
     end
 
     describe "POST #create" do
       context "with valid params" do
-        it "creates a new Comment" do
-          expect {post :create, params: {:pin_id => pin.to_param, comment: valid_attributes} }.to change(Comment, :count).by(1)
+        it "creates a new Pin" do
+          expect {post :create, params: {:id => pin.to_param, pin: valid_attributes} }.to change(Pin, :count).by(1)
         end
 
-        it "assigns a newly created comment as @comment" do
-          post :create, params: {:pin_id => pin.to_param, comment: valid_attributes}
-          expect(assigns(:comment)).to be_a(Comment)
-          expect(assigns(:comment)).to be_persisted
+        it "assigns a newly created pin as @pin" do
+          post :create, params: {:id => pin.to_param, pin: valid_attributes}
+          expect(assigns(:pin)).to be_a(Pin)
+          expect(assigns(:pin)).to be_persisted
         end
 
-        it "redirects to the created comment" do
-          post :create, params: {:pin_id => pin.to_param, comment: valid_attributes}
+        xit "redirects to the created pin" do
+          post :create, params: {:id => pin.to_param, pin: valid_attributes}
           expect(response).to redirect_to(pin_path(pin))
         end
       end
 
       context "with invalid params" do
-        it "assigns a newly created but unsaved comment as @comment" do
-          post :create, params: {:pin_id => pin.to_param, comment: invalid_attributes}
-          expect(assigns(:comment)).to be_a_new(Comment)
+        it "assigns a newly created but unsaved pin as @pin" do
+          post :create, params: {:id => pin.to_param, pin: invalid_attributes}
+          expect(assigns(:pin)).to be_a_new(Pin)
         end
 
         it "re-renders the 'new' template" do
-          post :create, params: {:pin_id => pin.to_param, comment: invalid_attributes}
+          post :create, params: {:id => pin.to_param, pin: invalid_attributes}
           expect(response).to render_template("new")
         end
       end
@@ -73,49 +73,49 @@ RSpec.describe PinssController, type: :controller do
 
     describe "PUT #update" do
       context "with valid params" do
-        it "updates the requested comment" do
-          comment = Comment.create! valid_attributes
-          put :update, params: {:pin_id => pin.to_param, id: comment.to_param, comment: valid_attributes}
-          comment.reload
+        xit "updates the requested pin" do
+          pin = Pin.create! valid_attributes
+          put :update, params: {id: pin.to_param, pin: valid_attributes}
+          pin.reload
           skip("Add assertions for updated state")
         end
 
-        it "assigns the requested comment as @comment" do
-          comment = Comment.create! valid_attributes
-          put :update, params: {:pin_id => pin.to_param, id: comment.to_param, comment: valid_attributes}
-          expect(assigns(:comment)).to eq(comment)
+        it "assigns the requested pin as @pin" do
+          pin = Pin.create! valid_attributes
+          put :update, params: {id: pin.to_param, pin: valid_attributes}
+          expect(assigns(:pin)).to eq(pin)
         end
 
-        it "redirects to the comment" do
-          put :update, params: {:pin_id => pin.to_param, id: comment.to_param, comment: valid_attributes}
+        it "redirects to the pin" do
+          put :update, params: {id: pin.to_param, pin: valid_attributes}
           expect(response).to redirect_to(pin_path(pin))
         end
       end
 
       context "with invalid params" do
-        it "assigns the comment as @comment" do
-          put :update, params: {:pin_id => pin.to_param, id: comment.to_param, comment: invalid_attributes}
-          expect(assigns(:comment)).to eq(comment)
+        it "assigns the pin as @pin" do
+          put :update, params: {id: pin.to_param, pin: invalid_attributes}
+          expect(assigns(:pin)).to eq(pin)
         end
 
         # I don't understand why this test is not passing
         xit "re-renders the 'edit' template" do
-          put :update, params: {:pin_id => pin.to_param, id: comment.to_param, comment: invalid_attributes}
-          expect(response).to render_template(edit_pin_comment_path(pin, comment))
+          put :update, params: {id: pin.to_param, pin: invalid_attributes}
+          expect(response).to render_template(edit_pin_pin_path(pin))
         end
       end
     end
 
     describe "DELETE #destroy" do
-      it "destroys the requested comment" do
+      it "destroys the requested pin" do
         expect {
-          delete :destroy, params: {:pin_id => pin.to_param, id: comment.to_param}
-        }.to change(Comment, :count).by(-1)
+          delete :destroy, params: {id: pin.to_param}
+        }.to change(Pin, :count).by(-1)
       end
 
-      it "redirects to the comments list" do
-        delete :destroy, params: {:pin_id => pin.to_param, id: comment.to_param}
-        expect(response).to redirect_to(pin_path(pin))
+      it "redirects to the pins list" do
+        delete :destroy, params: {id: pin.to_param}
+        expect(response).to redirect_to(pins_path)
       end
     end
   end
