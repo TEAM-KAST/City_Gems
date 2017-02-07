f64699cf3f2834d679cb12296745417722bb5b10
Kate Kourbatova <email": null>
2017-02-07 14:53:56 -0800

    Fix formatting issue in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,19 @@ class User < ApplicationRecord
   has_many :commented_pins, -> { distinct }, through: :comments, source: :pin
 
   def self.update_or_create(auth)
-      user = User.find_by(uid: auth[:uid]) || User.new
-      user.attributes = {
-        provider: auth[:provider],
-        uid: auth[:uid],
-        email: auth[:info][:email],
-        first_name: auth[:info][:first_name],
-        last_name: auth[:info][:last_name],
-        token: auth[:credentials][:token],
-        refresh_token: auth[:credentials][:refresh_token],
-        oauth_expires_at: auth[:credentials][:expires_at]
-      }
-      user.save
-      user
-    end
+    user = User.find_by(uid: auth[:uid]) || User.new
+    user.attributes = {
+      provider: auth[:provider],
+      uid: auth[:uid],
+      email: auth[:info][:email],
+      first_name: auth[:info][:first_name],
+      last_name: auth[:info][:last_name],
+      token: auth[:credentials][:token],
+      refresh_token: auth[:credentials][:refresh_token],
+      oauth_expires_at: auth[:credentials][:expires_at]
+    }
+    user.save
+    user
+  end
 
 end
