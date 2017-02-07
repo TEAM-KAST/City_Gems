e254ff4ee10b0c1e943b1bbad72566e399c0eb09
Deel With It <email": null>
2017-02-06 19:08:15 -0800

    merging to rebase with master

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,21 +1,22 @@
-// $(document).ready(function() {
-//   $('#map-placeholder').on('click', '#new-gem-button', function(event){
-//     event.preventDefault();
-//     var $target = $(event.target)
-//     var url = ($target.attr('href'))
-//     var $targetParent = ($target.parent())
-//
-//     $.ajax({
-//       url: url,
-//       method: 'GET',
-//       success: function(response) {
-//         $($targetParent).html(response)
-//
-//
-//
-//       }
-//     })
-//
-//
-//   });
-// });
+$(document).ready(function() {
+  $('#map-placeholder').on('click', '#new-gem-button', function(event){
+    event.preventDefault();
+    var $target = $(event.target)
+    var url = ($target.attr('href'))
+    var $targetParent = ($target.parent())
+    console.log(url)
+
+    $.ajax({
+      url: url,
+      method: 'GET',
+      success: function(response) {
+        $($targetParent).html(response)
+
+
+
+      }
+    })
+
+
+  });
+});
