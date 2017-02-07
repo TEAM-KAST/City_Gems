83fc44a4ec8f6fdf4148a590125344f95072c9e4
groovestation31785 <email": null>
2017-02-06 19:03:47 -0800

    Add styling for new pin form and add ajax request

--- app/assets/javascripts/new_pin.js
+++ app/assets/javascripts/new_pin.js
@@ -1,18 +1,42 @@
 $(document).ready(function() {
-  console.log("I'm LISTENING!!!!")
   $("#map-placeholder").on("click", "#new-gem-button", function(event){
-      console.log("I LISTENED!!!!")
-      event.preventDefault()
-      var url = $(event.target).attr('href');
-      console.log(url);
-        $.ajax({
-          url: url,
-          type: 'get'
-        }).done(function(data){
-          console.log(data)
-          var popupForm = $(data).children('section')
-          $('body').append(popupForm)
-          $('.mapboxgl-popup').remove();
-        })
-      })
-    })
+    event.preventDefault();
+    var url = $(event.target).attr('href');
+      $.ajax({
+        url: url,
+        type: 'get'
+      }).done(function(data){
+        var $popupForm = $(data).children('section');
+        $('body').append($popupForm);
+        // Global Variable from _map.html
+        if(addGemPopup){
+          addGemPopup.remove();
+          addGemPopup = null;
+        }
+      });
+    });
+
+
+// We don't really need this
+    $('body').on("submit", "#new_pin", function(event) {
+      // event.preventDefault();
+      // var $form = $(this);
+      // var data = $form.serialize();
+      // console.log(data)
+      // $.ajax({
+      //   type: 'post',
+      //   url: '/pins',
+      //   data: data
+      // }).done(function(response) {
+      //   console.log("DONE", $('.popup-new-pin-form'))
+      //   var $section = $form.closest('section.popup-new-pin-form');
+      //   $section.remove();
+      //
+      //
+      // }).error(function(response) {
+      //   console.log("ERROR", response);
+      // })
+    });
+
+
+  });
