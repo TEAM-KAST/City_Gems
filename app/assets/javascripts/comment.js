4434d3f746afd51c1360005c00a3710fb1617447
Deel With It <email": null>
2017-02-08 22:35:42 -0800

    add additional div elements to show page for pin, creates event handlers for new comments, as well as editing. Corrects bug with div for show page that would displace comments outside of itself.

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#gem-show-container').on('click',  function(event){
+  $('#gem-show-container').on('click', 'a', function(event){
     event.preventDefault();
     var $target = $(event.target)
     var url = ($target.attr('href'));
@@ -15,26 +15,40 @@ $(document).ready(function() {
   $('body').on('submit', '.edit_comment', function(event){
     event.preventDefault();
     var $target = $(event.target)
-    var url = $('body').children('form')["0"].getAttribute('action')
+    var $url = $('body').children('form')["0"].getAttribute('action')
     var data = $target.serialize()
-    console.log('URL', url)
     var $form = $(this).closest('form');
     $.ajax({
-      url: url,
+      url: $url,
       method: "PUT",
       data: data,
       dataType: 'JSON'
     }).done(function(data){
-      debugger;
-      var $old_content = $('#comment-field').children().children('span')
-      $($old_content).html(data.content)
+      var $commentId = $('body').children('form')["0"].getAttribute('action').substring(17)
+      var spanId = "#comment-"+ $commentId
+      $(spanId).html(data.content)
       $target.remove();
-      console.log($target)
-      console.log(data.content)
-
     })
   })
 
+  $('body').on('submit', '#new_comment', function(event){
+    event.preventDefault();
+    var $form = $(this);
+    var data = $form.serialize();
+    var $target = $(event.target)
+    var $url = $('body').children('form')["0"].getAttribute('action');
+    $.ajax ({
+      url: $url,
+      method: "POST",
+      data: data,
+      dataType: 'html'
+    }).done(function(response){
+      var $commentToPost = $(response).children('#comments-container').children('#comment-start:last')
+      $('#comments-container').prepend($commentToPost)
+      $target.remove();
+
+    })
+  })
 
 
 });
