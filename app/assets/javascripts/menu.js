bd00da4c27b64479be155c6b054743670235de60
Kate Kourbatova <email": null>
2017-02-08 16:01:21 -0800

    Implement slide toggle for menu

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,15 +1,22 @@
-$(document).ready(function() {
-  $('#menu_icon').on('click', function(event){
-    event.preventDefault();
-    var $target = $(event.target)
-    var url = ($target.attr('href'));
-    $.ajax ({
-      url: url,
-      method: "GET"
-    }).done(function(data){
-      console.log(data)
-      // var $form = $((data).closest('#id'));
-      $('body').append($form)
-    })
-  })
+// $(document).ready(function() {
+//   $('#menu_icon').on('click', function(event){
+//     event.preventDefault();
+//     var $target = $(event.target)
+//     var url = ($target.attr('href'));
+//     $.ajax ({
+//       url: url,
+//       method: "GET"
+//     }).done(function(data){
+//       console.log(data)
+//       // var $form = $((data).closest('#id'));
+//       $('body').append($form)
+//     })
+//   })
+// });
+
+$(document).ready(function(){
+    $("#menu-icon").click(function(){
+      event.preventDefault();
+      $("#menu-list").slideToggle();
+    });
 });
