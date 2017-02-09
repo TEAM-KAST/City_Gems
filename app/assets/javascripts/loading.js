3b18775f7a89930fca50c43f7ed1f670e6f6c327
groovestation31785 <email": null>
2017-02-08 23:22:24 -0800

    Rebase with master

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,12 +1,12 @@
-document.onreadystatechange = function () {
-  var state = document.readyState
-  if (state == 'interactive') {
-       document.getElementById('page-contents').style.visibility="hidden";
-  } else if (state == 'complete') {
-      setTimeout(function(){
-         document.getElementById('interactive');
-         document.getElementById('load').style.visibility="hidden";
-         document.getElementById('page-contents').style.visibility="visible";
-      },1000);
-  }
-}
+// document.onreadystatechange = function () {
+//   var state = document.readyState
+//   if (state == 'interactive') {
+//        document.getElementById('page-contents').style.visibility="hidden";
+//   } else if (state == 'complete') {
+//       setTimeout(function(){
+//          document.getElementById('interactive');
+//          document.getElementById('load').style.visibility="hidden";
+//          document.getElementById('page-contents').style.visibility="visible";
+//       },1000);
+//   }
+// }
